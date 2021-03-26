36b4ed34419d97ffb9106402292ea60ccf60ffcd
Code Sandbox <hello@codesandbox.io>
2021-03-26 18:43:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f49d9d5b1345e5321a20107a28d1622e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1635854fb3fb95f3a7c4fc79f239a875",
+    "url": "https://disco195.github.io/csb-k7fo1/index.html"
+  },
+  {
+    "revision": "99e582e69e8fa3a7199b",
+    "url": "https://disco195.github.io/csb-k7fo1/static/css/main.4e365974.chunk.css"
+  },
+  {
+    "revision": "112120bd4c2b49695d1f",
+    "url": "https://disco195.github.io/csb-k7fo1/static/js/2.cca168b8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://disco195.github.io/csb-k7fo1/static/js/2.cca168b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e582e69e8fa3a7199b",
+    "url": "https://disco195.github.io/csb-k7fo1/static/js/main.90c8e9c7.chunk.js"
+  },
+  {
+    "revision": "369dfefb0580d57b965c",
+    "url": "https://disco195.github.io/csb-k7fo1/static/js/runtime-main.3ea44b6e.js"
+  }
+]);
